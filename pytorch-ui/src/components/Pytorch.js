b30b3b8010767fda7b6b80624aea00a616194c32
Sohail Chauhan <sohail.chauhan@coradius.io>
2022-03-27 19:13:42 -0600

    Added CSS

--- pytorch-ui/src/components/Pytorch.js
+++ pytorch-ui/src/components/Pytorch.js
@@ -10,36 +10,39 @@ function Pytorch() {
     
                 <div className='mainBox'>
                     <div className='row'>
-                        <button className='elements-left cursorPointor' disabled ={true} onClick={() =>{
-                            console.log("hh")
-                        }} >PyTorch Build</button> 
+                        <p className='elements-left' >PyTorch Build</p> 
                         <p className='elements-right cursorPointor' >Stable(1.11.0)</p>
                         <p className='elements-right cursorPointor'>Preview(Nightly)</p>
                         <p className='elements-right cursorPointor'>LTS(1.8.2)</p>
                     </div>
-                    <div className='row' style={{visibility:"hidden"}}>
-                        <p className='elements-left cursorPointor'>PyTorch Build</p> 
-                        <p className='elements-right cursorPointor'>Stable(1.11.0)</p>
-                        <p className='elements-right cursorPointor'>Preview(Nightly)</p>
-                        <p className='elements-right cursorPointor'>LTS(1.8.2)</p>
+                    <div className='row'>
+                        <p className='elements-left cursorPointor'>Your OS</p> 
+                        <p className='elements-right cursorPointor'>Linux</p>
+                        <p className='elements-right cursorPointor'>Mac</p>
+                        <p className='elements-right cursorPointor'>Windows</p>
                     </div>
                     <div className='row'>
-                        <p className='elements-left cursorPointor'>PyTorch Build</p> 
-                        <p className='elements-right cursorPointor'>Stable(1.11.0)</p>
-                        <p className='elements-right cursorPointor'>Preview(Nightly)</p>
-                        <p className='elements-right cursorPointor'>LTS(1.8.2)</p>
+                        <p className='elements-left cursorPointor'>Language</p> 
+                        <p className='elements-right cursorPointor'>Python</p>
+                        <p className='elements-right cursorPointor'>C++/Java</p>
                     </div>
                     <div className='row'>
-                        <p className='elements-left cursorPointor'>PyTorch Build</p> 
-                        <p className='elements-right cursorPointor'>Stable(1.11.0)</p>
-                        <p className='elements-right cursorPointor'>Preview(Nightly)</p>
-                        <p className='elements-right cursorPointor'>LTS(1.8.2)</p>
+                        <p className='elements-left cursorPointor'>Package</p> 
+                        <p className='elements-right cursorPointor'>Conda</p>
+                        <p className='elements-right cursorPointor'>Pip</p>
+                        <p className='elements-right cursorPointor'>LibTorch</p>
+                        <p className='elements-right cursorPointor'>Source</p>
                     </div>
                     <div className='row'>
-                        <p className='elements-left cursorPointor'>PyTorch Build</p> 
-                        <p className='elements-right cursorPointor'>Stable(1.11.0)</p>
-                        <p className='elements-right cursorPointor'>Preview(Nightly)</p>
-                        <p className='elements-right cursorPointor'>LTS(1.8.2)</p>
+                        <p className='elements-left cursorPointor'>Compute Platform</p> 
+                        <p className='elements-right cursorPointor'>CUDA 10.2</p>
+                        <p className='elements-right cursorPointor'>CUDA 11.3</p>
+                        <p className='elements-right cursorPointor'>ROCM 4.2(beta)</p>
+                        <p className='elements-right cursorPointor'>CPU</p>
+                    </div>
+                    <div className='row'>
+                        <div className='elements-left cursorPointor'>Run this Command</div> 
+                        <p className='elements-right run-command cursorPointor'></p>
                     </div>
                 </div>
             </div>
