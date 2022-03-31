edca0a81a12a74f144c51fa3388e7d7906a38358
tauheedmaniar <tauheedmaniar54@gmail.com>
2022-03-30 23:20:32 -0600

    Chnaes new

--- pytorch-ui/src/components/PytorchDashboard.js
+++ pytorch-ui/src/components/PytorchDashboard.js
@@ -5,50 +5,159 @@ export default class PytorchDashboard extends Component {
         super(props)  
         
         this.state = {
-            showMe:false,
-            showME1:false,
-            showME2:false,
-            showMe3:false,
-            showMe4:false,
+            showOS:false,
+            showPackages:false,
+            showLanguages:false,
+            showCompute:false,
+            showDisplayed:false,
             buildpy:"",
+            yourOS :"",
+            packages:"",
+            language:"",
+            computePlaform: "",
+            displayRunCommand:"",
+            computes:"Cuda 11.3",
+            backgroundColor:"",
+            disableds:false,
         }
     }
 
 
-    onShowElement = (e)=>{
-        console.log(e.target.id)
+    onShowBuild = (e)=>{
+       // console.log(e.target.id)
+       console.log(e.target.id)
+
+       if(e.target.id === 'Preview'){
         this.setState({
             buildpy:e.target.id,
-            showMe:true
-        })
+            computes:'Cuda 11.1',
+            showOS:true,
+            backgroundColor:"bgColor"
+         }, ()=>{
+             this.ongetRunCommand()
+         })
+
+
+       }
+       else {
+        this.setState({
+            buildpy:e.target.id,
+            showOS:true,
+            computes:'Cuda 11.1',
+           backgroundColor:"bgColor"
+
+         }, ()=>{
+             this.ongetRunCommand()
+         })
+
+
+
+       }
+
+
+     
+        
 
     }
 
     
-    onShows = () =>{
-        console.log("s ")
+    onShowOS = (e) =>{
+        
+       // console.log("s ")
+
+
+        if(e.target.id === "Mac"  && (this.state.buildpy === "stable" || this.state.buildpy ===  "Preview" || this.state.buildpy === 'LTS') ){
+
+            console.log(e.target.id)
+            this.setState({
+                yourOS:e.target.id,
+                showLanguages:true,
+                disableds:true
+            },()=>{
+                this.ongetRunCommand()
+            })
+        
+        }else if(e.target.id === "Window"  && (this.state.buildpy === "stable" || this.state.buildpy ===  "Preview" || this.state.buildpy === 'LTS') ){
+
+            console.log(e.target.id)
+            this.setState({
+                yourOS:e.target.id,
+                showLanguages:true,
+                disableds:true
+            },()=>{
+                this.ongetRunCommand()
+            })
+        }
+
+        else{
+            console.log(e.target.id)
+            this.setState({
+                yourOS:e.target.id,
+                showLanguages:true,
+                disableds:false
+            },()=>{
+                this.ongetRunCommand()
+            })
+
+        }
+
+      
+    }
+
+    onShowPackage = (e) =>{
         this.setState({
-            showME1:true
+            packages:e.target.id,
+            showCompute:true
+        }, ()=>{
+            this.ongetRunCommand()
         })
     }
 
-    onShowTHird = () =>{
+    onShowLanguages = (e) =>{
         this.setState({
-            showME2:true
-        })
-    }
-
-    onShowFourth = () =>{
-        this.setState({
-            showMe3:true
+            language:e.target.id,
+            showPackages:true
+        },()=>{
+            this.ongetRunCommand()
         })
     }
   
-    onShowFive= () =>{
+    onShowPlatform= (e) =>{
         this.setState({
-            showMe4:true
+            computePlaform: e.target.id,
+            showDisplayed:true
+        }, ()=>{
+            this.ongetRunCommand()
         })
+
     }
+
+
+    ongetRunCommand = () =>{
+        
+      //  let [buildpy,yourOS,packages,language,computePlaform] =[...this.state]
+      console.log("followup",this.state.buildpy,this.state.yourOS,this.state.packages,this.state.language,this.state.computePlaform)
+       
+      if(this.state.buildpy === "stable" && this.state.yourOS === "linux" && this.state.packages === "conda" && this.state.language === "Python" && this.state.computePlaform === "Cuda10")
+        {
+            console.log("sux")
+            this.setState({
+                displayRunCommand:"Install this command"
+            })
+            
+        } else  if(this.state.buildpy === "stable" && this.state.yourOS === "Mac" && this.state.packages === "conda" && this.state.language === "Python" && this.state.computePlaform === "Cuda10")
+        {
+            console.log("sux")
+            this.setState({
+                displayRunCommand:"Install this command for the mac"
+            })
+            
+        } 
+         
+    }
+
+
+
   
     // onPyBuild =(e)=>{
 
@@ -61,6 +170,8 @@ export default class PytorchDashboard extends Component {
 
     render() {
 
+        let yles = this.state.backgroundColor;
+
     return (
         <>    
             <div>
@@ -70,48 +181,51 @@ export default class PytorchDashboard extends Component {
                         <p className='elements-left cursorPointor' disabled ={true} onClick={() =>{
                             console.log("hh")
                         }} >PyTorch Build</p> 
-                        <p className='elements-right cursorPointor' onClick={this.onShowElement} id="stable" value={this.state.buildpy}>Stable(1.11.0)</p>
-                        <p className='elements-right cursorPointor' onClick={this.onShowElement} id="Preview">Preview(Nightly)</p>
-                        <p className='elements-right cursorPointor'   style={{  width: "111px"}} onClick={this.onShowElement} id="LTS" >LTS(1.8.2)</p>
+                        <p className={`elements-right cursorPointor ${(this.state.buildpy==='stable')? 'bgColor': ""}`}  onClick={this.onShowBuild} id="stable" value={this.state.buildpy}>Stable(1.11.0)</p>
+                        <p className={`elements-right cursorPointor ${(this.state.buildpy==='Preview')? 'bgColor': ""}`}  onClick={this.onShowBuild} id="Preview">Preview(Nightly)</p>
+                        <p className={`elements-right cursorPointor ${(this.state.buildpy==='LTS')? 'bgColor': ""}`}   style={{  width: "111px"}} onClick={this.onShowBuild} id="LTS" >LTS(1.8.2)</p>
                     </div>
                     
-                    {this.state.showMe?   <div className='row'>
+                    {this.state.showOS?   <div className='row'>
                         <p className='elements-left cursorPointor' style={{width:"95px"}} >Your OS</p> 
-                        <p className='elements-right cursorPointor' onClick={this.onShows}>Linux</p>
-                        <p className='elements-right cursorPointor' onClick={this.onShows}>Mac</p>
-                        <p className='elements-right cursorPointor' style={{  width: "111px"}} onClick={this.onShows}>Windows</p>
+                        <p className={`elements-right cursorPointor ${(this.state.yourOS==='linux')? 'bgColor': ""}`}  id="linux" onClick={this.onShowOS}>Linux</p>
+                        <p className={`elements-right cursorPointor ${(this.state.yourOS==='Mac')? 'bgColor': ""}`}  id="Mac" onClick={this.onShowOS}>Mac</p>
+                        <p className={`elements-right cursorPointor ${(this.state.yourOS==='Window')? 'bgColor': ""}`}  id="Window" style={{  width: "111px"}} onClick={this.onShowOS}>Windows</p>
                     </div>: null }
 
+
+                    { this.state.showLanguages?   <div className='row'>
+                        <p className='elements-left cursorPointor' onClick={this.onShowPackage}>Language</p> 
+                        <p className={`elements-right cursorPointor ${(this.state.language==='Python')? 'bgColor': ""}`}  id="Python" style={{width:"173px", marginLeft:"24px"}} onClick={this.onShowLanguages}>Python</p>
+                        <p className={`elements-right cursorPointor ${(this.state.language==='Java')? 'bgColor': ""}`}  id="Java" style={{  width: "192px"}} onClick={this.onShowLanguages}>C++/Java</p>
+                    </div>: null}
+                   
+
                    {
-                    this.state.showME1? 
+                    this.state.showPackages? 
                     <div className='row'>
-                    <p className='elements-left cursorPointor'  onClick={this.onShowTHird} >Package</p> 
-                    <p className='elements-right cursorPointor'   style={{width:"71px", marginLeft:"36px"}} onClick={this.onShowTHird}>Conda</p>
-                    <p className='elements-right cursorPointor'   style={{width:"fit-content"}} onClick={this.onShowTHird}>Pip</p>
-                    <p className='elements-right cursorPointor'  onClick={this.onShowTHird}>LibTorch</p>
-                    <p className='elements-right cursorPointor' style={{  width: "70px"}} onClick={this.onShowTHird}>Source</p>
+                    <p className='elements-left cursorPointor'  onClick={this.onShowPackage} >Package</p> 
+                    <p className={`elements-right ${this.state.language==='Python'?'cursorPointor': 'disabled'} ${(this.state.packages==='conda')? 'bgColor': ""}`}  id="conda"  style={{width:"71px", marginLeft:"36px"}} onClick={this.onShowPackage}>Conda</p>
+                    <p className={`elements-right ${this.state.language==='Python'?'cursorPointor': 'disabled'} ${(this.state.packages==='Pip')? 'bgColor': ""}`}   id="Pip" style={{width:"fit-content"}} onClick={this.onShowPackage}>Pip</p>
+                    <p className={`elements-right ${this.state.language==='Java'?'cursorPointor': 'disabled'} ${(this.state.packages==='LibTorch')? 'bgColor': ""}`}   id="LibTorch" onClick={this.onShowPackage}>LibTorch</p>
+                    <p className={`elements-right ${this.state.language==='Python'?'cursorPointor': 'disabled'} ${(this.state.packages==='Source')? 'bgColor': ""}`}  id="Source" style={{  width: "70px"}} onClick={this.onShowPackage}>Source</p>
 
                 </div>:null
                    }
                     
-                    { this.state.showME2?   <div className='row'>
-                        <p className='elements-left cursorPointor' onClick={this.onShowTHird}>Language</p> 
-                        <p className='elements-right cursorPointor' style={{width:"173px", marginLeft:"24px"}} onClick={this.onShowFourth}>Python</p>
-                        <p className='elements-right cursorPointor' style={{  width: "192px"}} onClick={this.onShowFourth}>C++/Java</p>
-                    </div>: null}
                    
-                   {this.state.showMe3?  <div className='row' style={{height:"42px", marginLeft:"0px"}}>
+                   {this.state.showCompute?  <div className='row' style={{height:"42px", marginLeft:"0px"}}>
                         <p className='elements-left cursorPointor'>Compute Platform</p> 
-                        <p className='elements-right cursorPointor' style={{  marginLeft: "-36px", width:"auto"}} onClick={this.onShowFive}>Cuda 10.2</p>
-                        <p className='elements-right cursorPointor' style={{ width:"auto"}}>Cuda 11.3</p>
-                        <p className='elements-right cursorPointor' style={{ width:"auto"}}>ROCM 4.2(BETA)</p>
-                        <p className='elements-right cursorPointor'style={{ width:"10px"}}>CPU</p>
+                        <p className={`elements-right  ${this.state.yourOS === 'Mac'?'disabled': 'cursorPointor'} ${(this.state.computePlaform==='Cuda10')? 'bgColor': ""}`}   id="Cuda10" style={{  marginLeft: "-36px", width:"auto"}} onClick={this.onShowPlatform}>Cuda 10.2</p>
+                        <p className={`elements-right  ${this.state.yourOS === 'Mac'?'disabled': 'cursorPointor'} ${(this.state.computePlaform=== 'Cuda11')? 'bgColor': ""}`}  id="Cuda11" style={{ width:"auto"}} onClick={this.onShowPlatform}>Cuda11</p>
+                        <p className={`elements-right  ${(this.state.yourOS === 'Mac' || this.state.yourOS === 'Window' )?'disabled': 'cursorPointor'} ${(this.state.computePlaform==='ROCM')? 'bgColor': ""}`}  id="ROCM" style={{ width:"auto"}} onClick={this.onShowPlatform} >ROCM 4.2(BETA)</p>
+                        <p className={`elements-right   ${(this.state.computePlaform==='CPU')? 'bgColor': ""}`}   id="CPU" style={{ width:"10px"}} onClick={this.onShowPlatform} >CPU</p>
 
                     </div> : null}
                   
-                   { this.state.showMe4?   <div className='row'>
+                   { this.state.showDisplayed?   <div className='row'>
                         <p className='elements-left cursorPointor'>Run this Command:</p> 
-                        <input type="text" id="lname" name="lname"  className='commad-runs'/> 
+                        <input type="text" id="lname" name="lname"  value={this.state.displayRunCommand} className='commad-runs'/> 
                                           </div>: null}
                    
                 </div>
